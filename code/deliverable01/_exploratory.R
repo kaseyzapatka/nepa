c7896d7417eef0054353f836f252d712f1e6ff01
kaseyzapatka <kzapatka@gradcenter.cuny.edu>
2026-02-06 11:52:36 -0800

    update based on new file structure

--- code/deliverable01/_exploratory.R
+++ code/deliverable01/_exploratory.R
@@ -7,7 +7,7 @@
 # SETUP
 # --------------------------
 
-source(here::here("code", "deliverable1", "00_setup.R"))
+source(here::here("code", "deliverable01", "00_setup.R"))
 
 # --------------------------
 # LOAD SPECIFIC DATA
