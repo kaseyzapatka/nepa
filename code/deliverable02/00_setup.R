6dece4a5e80c447bd7ee9b39ddbae01898665d4c
kaseyzapatka <kzapatka@gradcenter.cuny.edu>
2026-02-26 12:11:20 -0800

    update sample function to include process type

--- code/deliverable02/00_setup.R
+++ code/deliverable02/00_setup.R
@@ -87,7 +87,7 @@ sample_review <- function(review_type_filter, process_type_filter = NULL) {
       review_type == review_type_filter,
       if (!is.null(process_type_filter)) process_type == process_type_filter else TRUE
     ) |>
-    select(project_id, project_type, project_review_type:review_type) |>
+    select(project_id, project_type, process_type, project_review_type:review_type) |>
     slice_sample(n = 1) |> 
     glimpse()
 }
